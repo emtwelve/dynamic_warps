8d30bded3a0ab25d0b3a29dcf54f02e98df7b6b3
nponte <nponte@andrew.cmu.edu>
2017-04-28 19:28:42 -0400

    Pushing the new algorith

--- code/src/make_graphcu.cu
+++ code/src/make_graphcu.cu
@@ -1,49 +1,31 @@
-/////// CREATE THE GRAPH ///////
-////////////////////////////////
+#define WARP_SIZE 32
 
-__device__ int
-find_total(int** input, int my_index, int other_index, int n_arg) {
+/////// CREATE THE ARGUMENT MATCHING ARRAY ///////
+//////////////////////////////////////////////////
+
+/* Find how many arguments I share with all the other tid */
+
+__global__ void
+find_grouping(int** input, int* arg_match, int tid, int num_arg, int N) {
+
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+
     int match = 0;
-    for (int i = 0; i < num_arg; i++) {
-        if (input[my_index] == input[other_index][arg_num]) {
-            match++;
+    if (idx < N) {
+        for (int i = 0; i < num_arg; i++) {
+            if (input[tid][i] == input[idx][i]) {
+                match++
+            }
         }
-    }
-    return match;
-}
-
-__global__ void
-graph_creator(int** input, int** graph, int n_arg, int N) {
-
-    int index = blockIdx.x * blockDim.x + threadIdx.x;
-
-    if (index < N) {
-        for (int i = 0; i < N; i++) {
-            graph[index][i] = find_total(input, index, i, num_arg);
-        }
+        arg_match[idx] = match;
     }
 }
 
-////////////////////////////////
-////////////////////////////////
+//////////////////////////////////////////////////
+//////////////////////////////////////////////////
 
-/////// OUTPUT THE GRAPH /////////
-//////////////////////////////////
-
-__global__ void
-graph_to_out_kernel(int** graph, int* device_out, int N, int n_arg) {
-
-    int index = blockIdx.x * blockDim.x + threadIdx.x;
-
-    if (index < N) {
-        for (int i = 0; i < N; i++) {
-            graph[index][i] = find_total(input, index, i, num_arg);
-
-}
-
-//////////////////////////////////
-//////////////////////////////////
-
+//////////// MAIN ALGORITHM /////////////
+/////////////////////////////////////////
 
 void create_Graph(int** input_array, int* result_array, int N, int num_arg)
 {
@@ -53,14 +35,70 @@ void create_Graph(int** input_array, int* result_array, int N, int num_arg)
     const int threadsPerBlock = 32;
     const int blocks = (N + threadsPerBlock - 1) / threadsPerBlock;
 
-    int** graph;
-    for (int
-    cudaMalloc((void**) & device_graph, N * (int**)
+    // Device Memory
+    int* device_arg_match;    // This is for the number of argument matches
+    cudaMalloc((void**) &device_arg_match, N * sizeof(int));
+    // 2-D array of the input values
+    int** device_input_array;
+    cudaMalloc((void**)&device_input_array, N*num_arg*sizeof(int));
+    cudaMemcpy(device_input_array, input_array, N*num_arg*sizeof(int), cudaMemcpyHostToDevice);
+    // Initialize the Thrust arrays
+    thrust::device_vector<int> index(N);
 
 
-    // Generate my Graph of the edges and weight
-    graph_creation_kernel<<blocks, threadsPerBlock>>(device_input, device_graph, N, n_arg);
-    cudaThreadSynchronize();
+    // Host Memory
+    int* result_array; // This is the final mapping of index to warp
+    bool* visited;      // This is for if a node is already assigned a warp
+    int num_mapped = 0; // How many have been mapped
+    int cur_index = 0;  // First tid in the current warp
 
-    // Create the output array
-    graph_to_out_kernel<<blocks, threadsPerBlock>>(device_graph, device_out, N, n_arg);
+    // Initiliaze our first tid in our first warp
+    visited[cur_index] = true;
+    result_array[num_mapped] = cur_index;
+    num_mapped++;
+
+    // Until I have filled all of my output array
+    while (num_mapped < N) {
+
+        // Find argument matches based on an index
+        find_arg_match<<blocks, threadsPerBlock>>(input_array, device_arg_match, cur_index, N);
+        cudaThreadSynchronize();
+
+
+        // Sort the index thrust array based on the value thrust array
+        thrust::sequence(index.begin(), index.end());
+        thrust::device_ptr<int> ptr_arg_match = thrust::device_pointer_cast(arg_match);
+        thrust::sort_by_key(ptr_arg_match, ptr_arg_match + N, index);
+
+        // Loop through the sorted list of arg match and pick the best 31
+        for (int i = 0; i < N; i++) {
+            // After sorting grab the 32 who arent visited yet
+            int pos_index = index[i];
+            // If its not already in a warp then put it in this warp
+            if (visited[pos_index] == false) {
+                visited[pos_index] = true;
+                result_array[num_mapped] = pos_index;
+                num_mapped++;
+            }
+
+            // If this warp has been filled move on
+            if (num_mapped % WARP_SIZE) {
+                break;
+            }
+        }
+
+        // Start a new warp
+        for (int i = 0; i < N; i++) {
+            // Find the first index with visited == false
+            if (visited[i] == false) {
+                cur_index = i;
+                visited[cur_index] = true;
+                result_array[num_mapped] = cur_index;
+                num_mapped++;
+            }
+        }
+    }
+}
+
+/////////////////////////////////////////
+/////////////////////////////////////////
